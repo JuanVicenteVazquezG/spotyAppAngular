d4e5c87319ee6894a9a6b4f0337f78918bc24d34
Juan Vicente Vazquez <juanvicentevazquezg@gmail.com>
2021-01-09 08:33:05 +0100

    preparing for routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
+import { ROUTES } from './app.routes';
+// Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
+// Components
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { ArtistComponent } from './components/artist/artist.component';
-import { NavbarComponent } from './share/navbar/navbar.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ArtistComponent,
-    NavbarComponent
+    NavbarComponent,
+    SearchComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(ROUTES, { useHash: true })],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
