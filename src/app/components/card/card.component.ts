92aace8e7ccb64a944ff8dd913f4f60e5b22bd96
Juan Vicente Vazquez <juanvicentevazquezg@gmail.com>
2021-01-10 21:37:53 +0100

    artist component creation

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,14 +1,21 @@
 import { Input } from '@angular/core';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styles: [],
 })
-export class CardComponent implements OnInit {
+export class CardComponent {
   @Input() items: any[] = [];
-  constructor() {}
 
-  ngOnInit(): void {}
+  constructor(private router: Router) {}
+
+  seeArtist = (item: any) => {
+    const artistId: string =
+      item.type === 'artist' ? item.id : item.artists[0].id;
+    console.log(artistId);
+    this.router.navigate(['artist', artistId]);
+  }
 }
